2cfc7e0ebc7440c200dd6562a871cc6d83f07398
doogyb <samueldoogan@gmail.com>
2023-04-14 16:55:59 +0100

    Set up Fastify skeleton server (#14)
    Also:
        - Remove node_modules relating to sockets
    
    closes #13

--- be/src/server.js
+++ be/src/server.js
@@ -1,6 +1,7 @@
 import cors from '@fastify/cors';
 import fastify from 'fastify';
-import fastifyIO from 'fastify-socket.io';
+
+import { routes } from './routes.js';
 
 const start = async () => {
 	const server = fastify();
@@ -9,20 +10,10 @@ const start = async () => {
 		origin: '*'
 	});
 
-	await server.register(fastifyIO, {
-		cors: {
-			origin: '*',
-			methods: ['GET', 'POST']
-		}
-	});
+	server.register(routes);
 
 	try {
 		await server.listen({ host: 'localhost', port: 3000 });
-		server.io.on('connection', socket => {
-			socket.on('data', integer => {
-				server.io.to(socket.id).emit('result', integer * 4);
-			});
-		});
 	} catch (err) {
 		server.log.error(err);
 		throw new Error(err);
