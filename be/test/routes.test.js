f4664a10913080218c94d330f0813c53c67fd89d
doogyb <samueldoogan@gmail.com>
2023-05-02 15:16:58 +0100

    Add histogram route (#23)
    closes #22

--- be/test/routes.test.js
+++ be/test/routes.test.js
@@ -14,19 +14,28 @@ const build_ = () => build({
 	}
 });
 
-const testRoute = async route => {
-	test(`/${route}`, async t => {
+const testRoute = async (route, additionalTests = _.identity) => {
+	test(`/${route}`, async tap => {
 		const server = await build_();
-		t.teardown(() => server.close());
+		tap.teardown(() => server.close());
 		const tests = await readDirFiles(`test/api/${route}`, '', JSON.parse);
 		for await (const { query, response: expectedResponse } of tests) {
 			const response = await server.inject(query);
-			t.equal(response.statusCode, 200, 'returns a 200 status code');
-			t.same(response.json(), expectedResponse, 'returns the expected response body');
+			tap.equal(response.statusCode, 200, 'returns a 200 status code');
+			const result = response.json();
+			tap.same(response.json(), expectedResponse, 'returns the expected response body');
+			additionalTests(tap, query, result, expectedResponse);
 		}
-		t.end();
+		tap.end();
 	});
 };
 
 testRoute('terms');
 testRoute('terms_terms');
+
+const histogramTests = async (tap, query, result) => {
+	const q = query.query;
+	const expectedBins = 'missing' in q ? q.bins + 1 : q.bins;
+	tap.equal(expectedBins, result.agg1.buckets.length);
+};
+testRoute('histogram', histogramTests);
