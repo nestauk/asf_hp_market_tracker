8819013f079824826d95d5c4cf79d0edd0f46566
doogyb <samueldoogan@gmail.com>
2023-05-11 16:38:46 +0100

    Add terms_histogram route
    closes #43

--- be/src/routes.js
+++ be/src/routes.js
@@ -2,11 +2,13 @@ import {
 	getDateHistogram,
 	getHistogram,
 	getTerms,
-	getTermsTerms
+	getTermsTerms,
+	getTerms_Histogram
 } from './routes/index.js';
 import {
 	dateHistogramSchema,
 	histogramSchema,
+	termsHistogramSchema,
 	termsSchema,
 	termsTermsSchema
 } from './schemas/index.js';
@@ -16,6 +18,7 @@ export const routes = (fastify, options, done) => {
 	fastify.get('/date_histogram', dateHistogramSchema, getDateHistogram);
 	fastify.get('/histogram', histogramSchema, getHistogram);
 	fastify.get('/terms', termsSchema, getTerms);
+	fastify.get('/terms_histogram', termsHistogramSchema, getTerms_Histogram);
 	fastify.get('/terms_terms', termsTermsSchema, getTermsTerms);
 
 	done();
--- be/src/routes/index.js
+++ be/src/routes/index.js
@@ -1,4 +1,5 @@
 export { getDateHistogram } from './date_histogram.js';
 export { getHistogram } from './histogram.js';
+export { getTerms_Histogram } from './terms_histogram.js';
 export { getTerms } from './terms.js';
 export { getTermsTerms } from './terms_terms.js';
