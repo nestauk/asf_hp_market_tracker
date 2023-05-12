8a8ccc0d35a2edf390c57035e4f06661bd48155d
doogyb <samueldoogan@gmail.com>
2023-05-12 14:44:39 +0100

    terms_stats route (#47)
    closes #45

--- be/src/schemas/index.js
+++ be/src/schemas/index.js
@@ -2,4 +2,5 @@ export { schema as dateHistogramSchema } from './date_histogram.js';
 export { schema as histogramSchema } from './histogram.js';
 export { schema as termsHistogramSchema } from './terms_histogram.js';
 export { schema as termsSchema } from './terms.js';
+export { schema as terms_StatsSchema } from './terms_stats.js';
 export { schema as termsTermsSchema } from './terms_terms.js';
