17de46eba1232e109678fe6519f41a5b729fa3fd
Luca Bonavita <mindrones@users.noreply.github.com>
2023-05-19 12:53:48 +0100

    Categories, stats views: add barchart
    closes #70

--- fe/src/lib/components/svizzle/Grid2Columns.svelte
+++ fe/src/lib/components/svizzle/Grid2Columns.svelte
@@ -2,33 +2,29 @@
 	import {makeStyleVars} from '@svizzle/dom';
 
 	export let gap;
-	export let percents = [20, 40, 40];
+	export let percents = [50, 50];
 
 	$: style = makeStyleVars({
 		gap,
 		p0: `${percents[0]}%`,
 		p1: `${percents[1]}%`,
-		p2: `${percents[2]}%`,
 	});
 </script>
 
-<div class='Grid3Columns' {style}>
+<div class='Grid2Cols' {style}>
 	<div class='col'>
 		<slot name='col0'>Missing column 0</slot>
 	</div>
 	<div class='col'>
 		<slot name='col1'>Missing column 1</slot>
 	</div>
-	<div class='col'>
-		<slot name='col2'>Missing column 2</slot>
-	</div>
 </div>
 
 <style>
-	.Grid3Columns {
+	.Grid2Cols {
 		display: grid;
 		gap: var(--gap);
-		grid-template-columns: var(--p0) var(--p1) var(--p2);
+		grid-template-columns: var(--p0) var(--p1);
 		height: 100%;
 		width: 100%;
 	}
