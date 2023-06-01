8fab1fda7ca0f8e63cabf1cb9b89a2e647e450ec
Luca Bonavita <mindrones@users.noreply.github.com>
2023-06-01 17:39:08 +0100

    layout changes

--- fe/src/lib/components/explorer/medium/CategoryGrid.svelte
+++ fe/src/lib/components/explorer/medium/CategoryGrid.svelte
@@ -7,7 +7,7 @@
 	export let domain;
 	export let items;
 
-	const maxSize = 8;
+	const maxSize = 20;
 	const computeSquareSize = value => maxSize * Math.sqrt(value / maxValue || 0);
 
 	const reshapeItems = _.mapWith(applyFnMap({
@@ -38,21 +38,29 @@
 </script>
 
 {#if doDraw}
-	<ol>
+	<!-- <ol>
 		{#each columnKeys as columnLabel}
 			<li>{columnLabel}</li>
 		{/each}
-	</ol>
+	</ol> -->
 
-	<div class='grid' style='--columns: {columnKeys.length}; --maxSize: {maxSize}px;'>
+	<div
+		class='grid'
+		style='--columns: {columnKeys.length}; --maxSize: {maxSize}px;'
+	>
 		{#each reshapedItems as {key, values}}
 			<div class='valueRow'>
 				{#each columnKeys as subKey}
 					{@const value = values?.[subKey]}
-					<div
-						class='square'
-						style='--width: {computeSquareSize(value)}px; background-color: {colorScale(value)};'
-					/>
+					<div class='box'>
+						<div
+							class='square'
+							style='
+								--width: {computeSquareSize(value)}px;
+								background-color: {colorScale(value)};
+							'
+						/>
+					</div>
 				{/each}
 			</div>
 			<div class='labelRow'>
@@ -64,29 +72,29 @@
 
 <style>
 	ol {
-		list-style: none;
 		counter-reset: li;
+		list-style: none;
 		padding-left: 0;
 	}
 
 	ol li {
-		position: relative;
-		padding-left: 2.5em;
 		margin-bottom: .5em;
+		padding-left: 2.5em;
+		position: relative;
 	}
 
 	ol li::before {
-		counter-increment: li;
-		content: counter(li);
-		position: absolute;
-		left: 0;
-		width: 1.6em;
-		height: 1.6em;
-		border: 2px solid white;
 		border-radius: 50%;
-		text-align: center;
-		line-height: 1.6em;
+		border: 2px solid white;
+		content: counter(li);
+		counter-increment: li;
 		font-size: .8em;
+		height: 1.6em;
+		left: 0;
+		line-height: 1.6em;
+		position: absolute;
+		text-align: center;
+		width: 1.6em;
 	}
 
 	.grid {
@@ -98,10 +106,25 @@
 		grid-template-columns: repeat(var(--columns), 1fr);
 		height: var(--maxSize);
 	}
+	.box {
+		align-items: end;
+		display: flex;
+		justify-content: center;
+	}
 	.square {
 		background-color: var(--colorPrimary);
 		background: white;
+		border: thin solid black;
 		height: var(--width);
 		width: var(--width);
+	}
+	.labelRow {
+		align-items: center;
+		border-top: thin solid lightgrey;
+		display: flex;
+		justify-content: center;
+		margin-bottom: 1em;
+		padding-top: 0.75em;
+		text-align: center;
 	}
 </style>
