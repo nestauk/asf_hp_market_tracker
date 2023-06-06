c399f42ff5bfed8af63835d0b7a478a4b2e5800f
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2023-06-06 15:49:56 -0300

    Add Grid3Rows, for CatGrid PR

---
+++ fe/src/lib/components/svizzle/Grid3Rows.svelte
@@ -0,0 +1,28 @@
+<script>
+	import {makeStyleVars} from '@svizzle/dom';
+
+	export let gap = 0;
+	export let dimensions = ['33%', '33%', '34%'];
+
+	$: style = makeStyleVars({
+		p0: dimensions[0],
+		p1: dimensions[1],
+		p2: dimensions[2],
+	});
+</script>
+
+<div class='Grid3Rows' {style}>
+	<slot />
+</div>
+
+<style>
+	.Grid3Rows {
+		display: grid;
+		grid-template-rows:
+			calc(var(--p0) - var(--gap) / 3)
+			calc(var(--p1) - var(--gap) / 3)
+			calc(var(--p2) - var(--gap) / 3);
+		height: 100%;
+		width: 100%;
+	}
+</style>
