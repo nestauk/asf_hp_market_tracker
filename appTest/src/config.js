a9d112cff04baed577edde6d5daf530582947609
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2023-06-08 15:28:59 -0300

    moved relevant config

---
+++ appTest/src/config.js
@@ -0,0 +1,7 @@
+/* testing */
+
+export const urlBases = {
+	development: 'http://localhost:5173',
+	preview: 'https://skeleton.netlify.app', // FIXME update
+	production: 'https://uk-ai-map.temp-domain', // TODO netlify
+};
