be01a73127feac0fdb561d4e54f972f874654013
doogyb <samueldoogan@gmail.com>
2023-06-30 11:02:32 +0100

    Switch to hpmt_gold_interim_v5
    closes #183
    
    Also:
    - keep only the most recent configs so that we can see how they change using git tools

--- be/src/conf.js
+++ be/src/conf.js
@@ -1,5 +1,5 @@
 export const domain = 'https://hpmt.es.dev.dap-tools.uk:9200';
-export const index = 'hpmt_gold_interim_v4';
+export const index = 'hpmt_gold_interim_v5';
 // fingerprint generated from cert file created during ES installation
 export const fingerprint = '8D:AC:F2:E3:92:C9:59:FB:E5:77:3A:82:3B:2E:11:C9:68:43:21:2E:CF:57:05:CB:5A:C6:84:12:E9:22:69:E4';
 
