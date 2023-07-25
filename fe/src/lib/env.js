075dd6abed75dc61eeab603a546489d56314006d
Luca Bonavita <mindrones@users.noreply.github.com>
2023-07-25 15:39:34 +0100

    Fix environments in netlify config file
    fixes #211

--- fe/src/lib/env.js
+++ fe/src/lib/env.js
@@ -1,7 +1,6 @@
 export const isDev = import.meta.env.DEV;
 
-// see `fe/netlify.toml`
-const backendEnv = import.meta.env?.VITE_BE_ENV || 'dev';
+const backendEnv = import.meta.env?.VITE_BE_ENV; // see `fe/netlify.toml`
 const beURLs = {
 	local: 'http://localhost:3000',
 	dev: 'https://hpmt.be.dev.dap-tools.uk',
