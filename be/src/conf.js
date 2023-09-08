1391d87932ef719795e22c7e51961d7ff05cdb5b
doogyb <samueldoogan@gmail.com>
2023-09-08 13:33:17 +0100

    Update logstash pipeline
    fixes #258
    closes #305
    closes #313

--- be/src/conf.js
+++ be/src/conf.js
@@ -6,7 +6,7 @@ const domains = {
 	production: 'https://hpmt.es.production.dap-tools.uk:9200'
 };
 export const domain = domains[esEnv];
-export const index = 'hpmt_gold_interim_v6';
+export const index = 'hpmt_gold_interim_v7';
 // fingerprint generated from cert file created during ES installation
 export const fingerprint = '8D:AC:F2:E3:92:C9:59:FB:E5:77:3A:82:3B:2E:11:C9:68:43:21:2E:CF:57:05:CB:5A:C6:84:12:E9:22:69:E4';
 
