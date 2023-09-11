8e7dfc92a4c62b27825054c86777b3ea6460e67a
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2023-09-11 01:12:58 -0300

    Added padding to `GridRows`
    Closes #299

--- fe/src/lib/components/svizzle/GridRows.svelte
+++ fe/src/lib/components/svizzle/GridRows.svelte
@@ -1,19 +1,32 @@
 <script>
+	import {makeStyleVars} from '@svizzle/dom';
+	export let gap;
+	export let padding;
 	export let rowLayout;
 
-	$:rowLayout = rowLayout || '1fr';
+	$: gap = gap || '0.25em';
+	$: padding = padding || '0.5em';
+	$: rowLayout = rowLayout || '1fr';
+	$: style = makeStyleVars({
+		gap,
+		padding,
+		rowLayout,
+	});
 </script>
 
-<div class='GridRows' style='grid-template-rows: {rowLayout};'>
+<div class='GridRows' {style}>
 	<slot />
 </div>
 
 <style>
 	div {
 		display: grid;
-		justify-items: center;
+		gap: var(--gap);
+		grid-template-rows: var(--rowLayout);
 		height: 100%;
+		justify-items: center;
 		overflow: hidden;
+		padding: var(--padding);
 		width: 100%;
 	}
 </style>
