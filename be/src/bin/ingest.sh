b6aa1eafef03f8149869dc528b56467288fac835
doogyb <samueldoogan@gmail.com>
2023-09-19 14:23:48 +0100

    add comments

--- be/src/bin/ingest.sh
+++ be/src/bin/ingest.sh
@@ -1,9 +1,8 @@
 if [ -z "$1" ]
-  then
+then
     echo "No index name supplied"
     exit 1
 fi
-
 
 cd pipeline
 export LOGSTASH_OUTPUT_INDEX=$1
@@ -11,15 +10,13 @@ source input.rc
 
 result=$(mysql --login-path=missions -e "use production_asf; SELECT COUNT(*) FROM hpmt_gold_interim;")
 count=$(echo $result | awk '{print $2}')
-count=49999
-echo $count
 
 for domain in dev staging production
 do 
     source $domain.rc
     export LOGSTASH_OUTPUT_DOMAIN="https://hpmt.es.$domain.dap-tools.uk"
-    echo "--- 0" > last_run.yml
-    last_sql_value=0
+    echo "--- 0" > last_run.yml # set value read by configuration to start at 0
+    last_sql_value=0 
     while [ $last_sql_value -le $count ]
     do
         if [[ "$domain" == "production" ]]
@@ -28,7 +25,7 @@ do
         else
             ./logstash -f /home/ubuntu/pipeline/hosted_pipeline.conf
         fi
-        last_sql_value=$(($last_sql_value + 50000))
+        last_sql_value=$(($last_sql_value + 50000)) # increase the number of rows already read
         echo "--- $last_sql_value" > last_run.yml
     done
 
