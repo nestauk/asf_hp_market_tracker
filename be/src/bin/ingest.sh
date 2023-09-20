2556d41d2c063aef8539d4325a4f0c609171d6c6
doogyb <samueldoogan@gmail.com>
2023-09-20 11:19:23 +0100

    script should turn aws ec2 instance on/off

--- be/src/bin/ingest.sh
+++ be/src/bin/ingest.sh
@@ -4,30 +4,17 @@ then
     exit 1
 fi
 
-cd pipeline
-export LOGSTASH_OUTPUT_INDEX=$1
-source input.rc
+export AWS_PAGER=""
 
-result=$(mysql --login-path=missions -e "use production_asf; SELECT COUNT(*) FROM hpmt_gold_interim;")
-count=$(echo $result | awk '{print $2}')
+instance="i-0c069e95aaa193572"
 
-for domain in dev staging production
-do 
-    source $domain.rc
-    export LOGSTASH_OUTPUT_DOMAIN="https://hpmt.es.$domain.dap-tools.uk"
-    echo "--- 0" > last_run.yml # set value read by configuration to start at 0
-    last_sql_value=0 
-    while [ $last_sql_value -le $count ]
-    do
-        if [[ "$domain" == "production" ]]
-        then
-            ./logstash -f /home/ubuntu/pipeline/managed_pipeline.conf
-        else
-            ./logstash -f /home/ubuntu/pipeline/hosted_pipeline.conf
-        fi
-        last_sql_value=$(($last_sql_value + 50000)) # increase the number of rows already read
-        echo "--- $last_sql_value" > last_run.yml
-    done
+aws ec2 start-instances --instance-ids $instance  --output=text
+aws ec2 wait instance-running --instance-ids=$instance
+sleep 10 # need to wait a little longer before being able to ssh
+public_ip=$(aws ec2 describe-instances --instance-ids $instance --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
 
-    echo "Finished"
-done
+scp -o StrictHostKeyChecking=no es/hosted_pipeline.conf ubuntu@$public_ip:/home/ubuntu/hpmt_pipeline/hosted_pipeline.conf
+scp -o StrictHostKeyChecking=no es/managed_pipeline.conf ubuntu@$public_ip:/home/ubuntu/hpmt_pipeline/managed_pipeline.confm1ss10n5
+ssh -o StrictHostKeyChecking=no ubuntu@$public_ip 'bash -s' < src/bin/ingestServerSide.sh $*
+
+aws ec2 stop-instances --instance-ids $instance --output=text
--- be/src/bin/syncPipelineConfs.sh
+++
@@ -1,2 +0,0 @@
-scp es/hosted_pipeline.conf ubuntu@13.40.13.228:/home/ubuntu/pipeline/hosted_pipeline.conf
-scp es/managed_pipeline.conf ubuntu@13.40.13.228:/home/ubuntu/pipeline/managed_pipeline.conf
