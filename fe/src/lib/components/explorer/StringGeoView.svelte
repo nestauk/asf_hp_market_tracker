360d0afcfc22fd3b3294df88ec1dd3ec3a195739
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2023-09-28 10:27:19 -0300

    removed unintended untouched event

--- fe/src/lib/components/explorer/StringGeoView.svelte
+++ fe/src/lib/components/explorer/StringGeoView.svelte
@@ -159,7 +159,6 @@
 						extentsType={$_selection.stackedBarsExtents}
 						groupSortBy={$_selection.stringsGeoSortBy}
 						on:barTouched={onBarHovered}
-						on:barUntouched={onBarExited}
 						shouldResetScroll={true}
 						slot='col1'
 						theme={$_stackedBarchartTheme}
