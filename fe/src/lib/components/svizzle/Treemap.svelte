255aeabcde28797716648529a68ecfe1931efd73
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2023-09-28 12:14:30 -0300

    Treemaps: Text now doesn't render if it doesn't fit.
    Closes #372

--- fe/src/lib/components/svizzle/Treemap.svelte
+++ fe/src/lib/components/svizzle/Treemap.svelte
@@ -1,4 +1,5 @@
 <script>
+	import {makeStyleVars} from '@svizzle/dom';
 	import {getKey, getValue} from '@svizzle/utils';
 	import {hierarchy, stratify, treemap} from 'd3-hierarchy';
 	import * as _ from 'lamb';
@@ -15,6 +16,9 @@
 	let treemapHeight;
 	let treemapWidth;
 
+	const lineHeight = 20;
+	const padding = 5;
+
 	const dispatch = createEventDispatcher();
 	const stratifyData = stratify().path(_.identity);
 
@@ -29,6 +33,22 @@
 		.paddingInner(paddingInner);
 	$: treeRoot = getTreeRoot(items);
 	$: treemapLeaves = getTreemap(treeRoot).leaves();
+	$: textGroup = textGroup || new Array(treemapLeaves.length);
+	$: rects = _.map(textGroup, gNode => gNode?.getBBox());
+	$: doesTextFit = _.map(
+		_.zip(rects, treemapLeaves),
+		([rect, {x0, x1, y0, y1}]) => {
+			if (!rect) return false;
+			return [
+				rect.width < x1 - x0 - padding * 2,
+				rect.width < y1 - y0 - padding * 2
+			];
+		}
+	);
+	$: style = makeStyleVars({
+		lineHeight: `${lineHeight}px`,
+		padding: `${padding}px`,
+	});
 </script>
 
 <div
@@ -39,9 +59,10 @@
 	<svg
 		height={treemapHeight}
 		width={treemapWidth}
+		{style}
 	>
 		{#each treemapLeaves
-			as {x0, x1, y0, y1, data: {data}}
+			as {x0, x1, y0, y1, data: {data}}, i
 		}
 			<g
 				on:mousemove={({x, y}) => dispatch('leafHovered', {data, x, y})}
@@ -62,16 +83,27 @@
 					stroke='var(--colorBorderAux)'
 					width={x1-x0}
 				/>
-				<text
-					dx={5}
-					dy={5}
-					fill={keyToColorLabelFn(keyAccessor(data))}
-				>{keyAccessor(data)}</text>
-				<text
-					dx={5}
-					dy={25}
-					fill={keyToColorLabelFn(keyAccessor(data))}
-				>{valueAccessor(data)}</text>
+				<g
+					bind:this={textGroup[i]}
+					transform={!doesTextFit[i][0] && doesTextFit[i][1]
+						? `translate(0,${y1-y0}) rotate(-90)`
+						: ''
+					}
+					class:tooLarge={!doesTextFit[i][0] && !doesTextFit[i][1]}
+				>
+					<text
+						dx={padding}
+						dy={padding}
+						fill={keyToColorLabelFn(keyAccessor(data))}
+						maxHeight={y1-y0}
+						maxWidth={x1-x0}
+					>{keyAccessor(data)}</text>
+					<text
+						dx={padding}
+						dy={padding + lineHeight}
+						fill={keyToColorLabelFn(keyAccessor(data))}
+					>{valueAccessor(data)}</text>
+				</g>
 			</g>
 		{/each}
 	</svg>
@@ -87,5 +119,9 @@
 	text {
 		dominant-baseline: hanging;
 		stroke: none;
+	}
+
+	g.tooLarge {
+		visibility: hidden;
 	}
 </style>
