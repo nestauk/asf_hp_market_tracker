5add3f374632ffcd9e63a3745f4165ea2c7bb936
Sebastian Ferreyra Pons <ushiferreyra@gmail.com>
2023-09-29 15:24:00 -0300

    reverted `env.js`

--- fe/src/lib/env.js
+++ fe/src/lib/env.js
@@ -3,7 +3,7 @@ export const isDev = import.meta.env.DEV;
 const backendEnv = import.meta.env?.VITE_BE_ENV || 'dev'; // see `fe/netlify.toml`
 const beURLs = {
 	local: 'http://localhost:3000',
-	dev: 'https://hpmt.be.staging.dap-tools.uk',
+	dev: 'https://hpmt.be.dev.dap-tools.uk',
 	staging: 'https://hpmt.be.staging.dap-tools.uk',
 	production: 'https://hpmt.be.production.dap-tools.uk'
 };
