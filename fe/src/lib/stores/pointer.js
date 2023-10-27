b2d20ca730725e0de37f120fdb864f2ffaa79999
Luca Bonavita <mindrones@users.noreply.github.com>
2023-10-27 18:27:12 +0100

    pointer.js: spaces to tabs

--- fe/src/lib/stores/pointer.js
+++ fe/src/lib/stores/pointer.js
@@ -2,69 +2,69 @@ import {writable} from 'svelte/store';
 import * as _ from "lamb";
 
 export const _pointerSupport = writable({
-    touch: false,
-    mouse: false,
-    both: false,
-    onlyTouch: false,
-    onlyMouse: false
+	both: false,
+	mouse: false,
+	onlyMouse: false,
+	onlyTouch: false,
+	touch: false,
 });
 
 const optionsByBaseMediaQuery = {
-    pointer: ['coarse', 'fine', 'none'],
-    hover: ['hover', 'none'],
+	hover: ['hover', 'none'],
+	pointer: ['coarse', 'fine', 'none'],
 };
 
 const optionsByMediaQuery = {
-    'pointer': optionsByBaseMediaQuery.pointer,
-    'any-pointer': optionsByBaseMediaQuery.pointer,
-    'hover': optionsByBaseMediaQuery.hover,
-    'any-hover': optionsByBaseMediaQuery.hover
+	'pointer': optionsByBaseMediaQuery.pointer,
+	'any-pointer': optionsByBaseMediaQuery.pointer,
+	'hover': optionsByBaseMediaQuery.hover,
+	'any-hover': optionsByBaseMediaQuery.hover
 };
 
 const mediaQueryListeners = new Map();
 
 const doMediaQueries = ([type, options]) => [
-    type,
-    _.pipe([
-        _.mapWith(option => {
-            const mq = matchMedia(`(${type}: ${option})`);
-            // Register new listener and store in map for later removal
-            mq.addEventListener('change', updateStore);
-            mediaQueryListeners.set(mq, updateStore);
-            return [option, mq.matches];
-        }),
-        _.fromPairs
-    ])(options)
+	type,
+	_.pipe([
+		_.mapWith(option => {
+			const mq = matchMedia(`(${type}: ${option})`);
+			// Register new listener and store in map for later removal
+			mq.addEventListener('change', updateStore);
+			mediaQueryListeners.set(mq, updateStore);
+			return [option, mq.matches];
+		}),
+		_.fromPairs
+	])(options)
 ];
 
 const getMediaQueries = _.pipe([
-    _.pairs,
-    _.mapWith(doMediaQueries),
-    _.fromPairs
+	_.pairs,
+	_.mapWith(doMediaQueries),
+	_.fromPairs
 ]);
 
 const unregisterMediaQueryListeners = () => {
-    mediaQueryListeners.forEach((listener, mq) => {
-        mq.removeEventListener('change', listener);
-    });
-    mediaQueryListeners.clear();
+	mediaQueryListeners.forEach((listener, mq) => {
+		mq.removeEventListener('change', listener);
+	});
+	mediaQueryListeners.clear();
 };
 
 const updateStore = () => {
-    unregisterMediaQueryListeners(); // Remove old listeners
+	unregisterMediaQueryListeners(); // Remove old listeners
 
-    const results = getMediaQueries(optionsByMediaQuery);
+	const results = getMediaQueries(optionsByMediaQuery);
 
-    const touch = results.pointer.coarse || results['any-pointer'].coarse;
-    const mouse = results.hover.hover || results['any-hover'].hover;
+	const touch = results.pointer.coarse || results['any-pointer'].coarse;
+	const mouse = results.hover.hover || results['any-hover'].hover;
 
-    _pointerSupport.set({
-        touch,
-        mouse,
-        both: touch && mouse,
-        onlyTouch: touch && !mouse,
-        onlyMouse: !touch && mouse,
-    });
+	_pointerSupport.set({
+		touch,
+		mouse,
+		both: touch && mouse,
+		onlyTouch: touch && !mouse,
+		onlyMouse: !touch && mouse,
+	});
 };
 
 // Initialize with the current media query state
